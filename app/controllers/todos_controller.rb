c40fef0f486f37741f811e79267cf0573665fa16
pauleatonc <pauleatonc@gmail.com>
2018-09-07 02:48:05 -0300

    entrega final

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,50 @@
+class TodosController < ApplicationController
+
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+      @todo = Todo.new(todo_params)
+      @todo.save
+      redirect_to todos_path
+  end
+
+def show
+  @todo = Todo.find(params[:id])
+end
+
+def edit
+  @todo = Todo.find(params[:id])
+end
+
+def update
+  @todo = Todo.find(params[:id])
+  @todo.update(todo_params)
+  redirect_to todos_path
+end
+
+def destroy
+  @todo = Todo.find(params[:id])
+  @todo.destroy
+  redirect_to todos_path
+end
+
+def validation
+  @todo = Todo.find(params[:id])
+  @todo.where(completed:true)
+  redirect_to todos_path
+end
+
+
+  private
+
+  def todo_params
+  params.require(:todo).permit(:description, :completed)
+  end
+
+end
